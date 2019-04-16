ea4ec7891e5e81cccf9e6ffbade59a7175046c2b
bren <bren@153.io>
2019-04-16 21:31:16 +0100

    write output html

--- packages/loeb/index.js
+++ packages/loeb/index.js
@@ -1,20 +1,38 @@
 const Bundler = require('parcel-bundler')
 const importFresh = require('import-fresh')
 const React = require('react')
-const {renderToStaticMarkup} = require('react-dom/server')
+const {renderToStaticNodeStream} = require('react-dom/server')
+const path = require('path')
+const fs = require('fs')
+const mkdirp = require('mkdirp')
 
 module.exports = async () => {
-	const bundler = new Bundler('pages/**/*.mdx', {
+	const bundler = new Bundler('pages/**/*', {
 		target: 'node',
 		hmr: false,
 		sourceMaps: false,
 	})
+
 	bundler.addAssetType('mdx', require.resolve('@mdx-js/parcel-plugin-mdx/src/MDXAsset'))
 
 	bundler.on('bundled', bundle => {
 		bundle.childBundles.forEach(entry => {
 			const Component = importFresh(entry.name).default
-			console.log(renderToStaticMarkup(React.createElement(Component)))
+			const targetPath = path.join(
+				'site',
+				path.relative('dist', entry.name).replace(/\.js$/, '.html')
+			)
+
+			mkdirp.sync(
+				path.dirname(targetPath)
+			)
+
+			renderToStaticNodeStream(React.createElement(Component)).pipe(
+				fs.createWriteStream(
+					targetPath,
+				)
+			)
+			
 		})
 	})
 
