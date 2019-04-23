ac340cec5b52c37a3bdb207b11a45dbd07b3f0b0
bren <bren@153.io>
2019-04-23 21:56:46 +0100

    more reasonable way of structuring things

--- packages/core/index.js
+++ packages/core/index.js
@@ -6,89 +6,83 @@ const importFresh = require('import-fresh')
 const fs = require('fs')
 const mkdirp = require('mkdirp')
 
+const commonOptions = {
+	target: 'node',
+	mode: 'development',
+	watch: true,
+	devtool: false,
+	output: {
+		libraryTarget: 'commonjs',
+	}
+}
+
 module.exports = async ({ watch = true, plugins = [] }) => {
 	const entries = new Set()
 	let ready = false
 	const watcher = chokidar.watch('./pages/**/*')
 
-	function build() {
-		if(ready) {
-			const compiler = webpack(merge.smart({
-				entry: Array.from(entries).reduce((entry, key) => ({
-					...entry,
-					[key]: './' + key
-				}), {}),
-				target: 'node',
-				mode: 'development',
-				watch: true,
-				devtool: false,
-				output: {
-					libraryTarget: 'commonjs',
-					path: path.resolve(process.cwd(), '.cache')
-				}
+	const compilers = new Map()
+
+	watcher.on('add', entry => {
+		if(!compilers.has(entry)) {
+			const output = {
+				path: path.resolve(process.cwd(), '.cache', path.dirname(entry)),
+				filename: path.basename(entry) + '.js'
+			}
+
+			const compiler = webpack(merge.smart(commonOptions, {
+				entry: `./${entry}`,
+				output
 			}, ...plugins.map(plugin => plugin.webpack)))
-			
-			compiler.run((err, stats) => {
+
+			compilers.set(entry, compiler)
+			compiler.watcher = compiler.watch({}, (err, stats) => {
 				if(err) console.error(err)
+				console.log(stats.toString({
+					colors: true
+				}) + '\n')
 
-				const {assetsByChunkName} = stats.toJson()
-				Object.keys(assetsByChunkName).forEach(entry => {
-					const asset = path.resolve(process.cwd(), '.cache', assetsByChunkName[entry])
-					const page = importFresh(asset).default
+				const asset = path.resolve(output.path, output.filename)
+				const page = importFresh(asset).default
 
-					const entryType = path.extname(entry).slice(1)
+				const entryType = path.extname(entry).slice(1)
 
-					const targetPath = path.join(
-						'site',
-						entry.replace(new RegExp(`${entryType}$`), 'html')
-					)
+				const targetPath = path.join(
+					'site',
+					entry.replace(new RegExp(`${entryType}$`), 'html')
+				)
 
-					mkdirp.sync(
-						path.dirname(targetPath)
-					)
+				mkdirp.sync(
+					path.dirname(targetPath)
+				)
 
-					const plugin = plugins.find(plugin => plugin.type === entryType)
+				const plugin = plugins.find(plugin => plugin.type === entryType)
 
-					if(!plugin) {
-						throw new Error(`no plugin to handle page ${entry}`)
-					}
+				if(!plugin) {
+					throw new Error(`no plugin to handle page ${entry}`)
+				}
 
-					const output = plugin.render(page)
+				const rendered = plugin.render(page)
 
-					if(output.pipe) {
-						output.pipe(
-							fs.createWriteStream(
-								targetPath,
-							)
+				if(rendered.pipe) {
+					rendered.pipe(
+						fs.createWriteStream(
+							targetPath,
 						)
-					} else if(typeof output === string || Buffer.isBuffer(output)) {
-						fs.writeFileSync(targetPath, output)
-					} else {
-						throw new Error(`plugin for ${entryType} should output a string, Buffer, or ReadableStream`)
-					}
-				})
+					)
+				} else if(typeof rendered === 'string' || Buffer.isBuffer(rendered)) {
+					fs.writeFileSync(targetPath, rendered)
+				} else {
+					throw new Error(`plugin for ${entryType} should output a string, Buffer, or ReadableStream`)
+				}
 			})
 		}
-	}
-
-	watcher.on('ready', () => {
-		ready = true
-		build()
-	})
-
-	watcher.on('add', path => {
-		entries.add(path)
-		build()
 	})
 
 	watcher.on('unlink', path => {
-		entries.delete(path)
-		build()
+		console.log(compilers)
+		const compiler = compilers.get(path)
+		compilers.delete(path)
+		compiler.watcher.close()
 	})
-
-	watcher.on('change', build)
-
-	// compiler.watch({}, (err, stats) => {
-	// 	console.log(err, stats)
-	// })
 }
